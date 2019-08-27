d2235fe2caa22ced45292ec269bdafac12fb3f4e
sy2es94098 <c44064080@mail.ncku.edu.tw>
2019-08-27 12:06:34 +0800

    Fix mathod findpath in role.java

--- src/blackAndWhite/Role.java
+++ src/blackAndWhite/Role.java
@@ -280,188 +280,221 @@ public class Role {
 		
 	}
 	
-
+	private void findPathUp(int x ,int y ,int type)
+	{
+		if(y != 0)
+		{
+			if(board[x][y-1] == type)
+			{
+				for(int k = y-1 ; k > -1 ; --k)
+				{
+					if(board[x][k] == 0)
+					{
+						board[x][k] = 4;
+						break;
+					}
+					else if(board[x][k] == type){
+						continue;
+					}
+					else {
+						break;
+					}
+				}
+			}
+		}
+		
+	}	
+	private void findPathDown(int x ,int y ,int type)
+	{
+		if(y !=  9)
+		{
+			if(board[x][y+1] == type)
+			{
+				for(int k = y+1 ; k < 10 ; ++k)
+				{
+					if(board[x][k] == 0)
+					{
+						board[x][k] = 4;
+						break;
+					}
+					else if(board[x][k] == type){
+						continue;
+					}
+					else {
+						break;
+					}
+				}
+			}
+		}
+	}	
+	private void findPathLeft(int x ,int y ,int type)
+	{
+		if(x != 0)
+		{
+			if(board[x-1][y] == type)
+			{
+				for(int k = x-1 ; k > -1 ; --k)
+				{
+					if(board[k][y] == 0)
+					{
+						board[k][y] = 4;
+						break;
+					}
+					else if(board[k][y] == type){
+						continue;
+					}
+					else {
+						break;
+					}
+				}
+			}
+		}
+	}	
+	private void findPathRight(int x ,int y ,int type)
+	{
+		if(x != 9)
+		{
+			if(board[x+1][y] == type)
+			{
+				for(int k = x+1 ; k < 10 ; ++k)
+				{
+					if(board[k][y] == 0)
+					{
+						board[k][y] = 4;
+						break;
+					}
+					else if(board[k][y] == type){
+						continue;
+					}
+					else {
+						break;
+					}
+				}
+			}
+		}
+	}	
+	
+	private void findPathRightAndUp(int x ,int y ,int type)
+	{
+		if( x != 9 && y != 0)
+		{
+			if(board[x+1][y-1] == type)
+			{
+				for(int k = 1;;++k)
+				{
+					if(x+k > 9 || y-k < 0)
+						break;
+					if(board[x+k][y-k] == 0)
+					{
+						board[x+k][y-k] = 4;
+						break;
+					}
+					else if(board[x+k][y-k] == type){
+						continue;
+					}
+					else {
+						break;
+					}
+					
+				}
+			}
+		}
+	}	
+	
+	private void findPathRightAndDown(int x ,int y ,int type)
+	{
+		if(x != 9 &&  y != 9)
+		{
+			if(board[x+1][y+1] == type)
+			{
+				for(int k = 1;;++k)
+				{
+					if(x+k > 9 || y+k > 9)
+						break;
+					if(board[x+k][y+k] == 0)
+					{
+						board[x+k][y+k] = 4;
+						break;
+					}
+					else if(board[x+k][y+k] == type){
+						continue;
+					}
+					else {
+						break;
+					}
+					
+				}
+			}
+		}
+	}	
+	
+	private void findPathLeftAndUp(int x ,int y ,int type)
+	{
+		if(x != 0 && y != 0)
+		{
+			if(board[x-1][y-1] == type)
+			{
+				for(int k = 1;;++k)
+				{				
+					if(x-k < 0 || y-k < 0)
+						break;
+					if(board[x-k][y-k] == 0)
+					{
+						board[x-k][y-k] = 4;
+						break;
+					}
+					else if(board[x-k][y-k] == type){
+						continue;
+					}
+					else {
+						break;
+					}
+	
+				}
+			}
+		}
+		
+	}	
+	
+	private void findPathLeftAndDown(int x ,int y ,int type)
+	{
+		if(x != 0 && y != 9)
+		{
+			if(board[x-1][y+1] == type)
+			{
+				for(int k = 1;;++k)
+				{
+					if(x-k < 0 || y+k > 9)
+						break;
+					if(board[x-k][y+k] == 0)
+					{
+						board[x-k][y+k] = 4;
+						break;
+					}
+					else if(board[x-k][y+k] == type){
+						continue;
+					}
+					else {
+						break;
+					}
+				}
+			}
+		}
+	}
+	
 	private void pathCheck(int x , int y, int type) {
 		for(int i = 0 ; i < 10 ; ++i)
 		{
 			for(int j = 0 ; j < 10 ; ++j)
 			{
-				if(x != 9)
-				{
-					if(board[x+1][y] == type)
-					{
-						for(int k = x+1 ; k < 10 ; ++k)
-						{
-							if(board[k][y] == 0)
-							{
-								board[k][y] = 4;
-								break;
-							}
-							else if(board[k][y] == type){
-								continue;
-							}
-							else {
-								break;
-							}
-						}
-					}
-				}
-				if(x != 0)
-				{
-					if(board[x-1][y] == type)
-					{
-						for(int k = x-1 ; k > -1 ; --k)
-						{
-							if(board[k][y] == 0)
-							{
-								board[k][y] = 4;
-								break;
-							}
-							else if(board[k][y] == type){
-								continue;
-							}
-							else {
-								break;
-							}
-						}
-					}
-				}
-				
-				if(y !=  9)
-				{
-					if(board[x][y+1] == type)
-					{
-						for(int k = y+1 ; k < 10 ; ++k)
-						{
-							if(board[x][k] == 0)
-							{
-								board[x][k] = 4;
-								break;
-							}
-							else if(board[x][k] == type){
-								continue;
-							}
-							else {
-								break;
-							}
-						}
-					}
-				}
-				if(y != 0)
-				{
-					if(board[x][y-1] == type)
-					{
-						for(int k = y-1 ; k > -1 ; --k)
-						{
-							if(board[x][k] == 0)
-							{
-								board[x][k] = 4;
-								break;
-							}
-							else if(board[x][k] == type){
-								continue;
-							}
-							else {
-								break;
-							}
-						}
-					}
-				}
-				
-				if(x != 9 &&  y != 9)
-				{
-					if(board[x+1][y+1] == type)
-					{
-						for(int k = 1;;++k)
-						{
-							if(x+k > 9 || y+k > 9)
-								break;
-							if(board[x+k][y+k] == 0)
-							{
-								board[x+k][y+k] = 4;
-								break;
-							}
-							else if(board[x+k][y+k] == type){
-								continue;
-							}
-							else {
-								break;
-							}
-							
-						}
-					}
-				}
-				
-				if(x != 0 && y != 0)
-				{
-					if(board[x-1][y-1] == type)
-					{
-						for(int k = 1;;++k)
-						{				
-							if(x-k < 0 || y-k < 0)
-								break;
-							if(board[x-k][y-k] == 0)
-							{
-								board[x-k][y-k] = 4;
-								break;
-							}
-							else if(board[x-k][y-k] == type){
-								continue;
-							}
-							else {
-								break;
-							}
-			
-						}
-					}
-				}
-				
-				if( x != 9 && y != 0)
-				{
-					if(board[x+1][y-1] == type)
-					{
-						for(int k = 1;;++k)
-						{
-							if(x+k > 9 || y-k < 0)
-								break;
-							if(board[x+k][y-k] == 0)
-							{
-								board[x+k][y-k] = 4;
-								break;
-							}
-							else if(board[x+k][y-k] == type){
-								continue;
-							}
-							else {
-								break;
-							}
-							
-						}
-					}
-				}
-				
-				if(x != 0 && y != 9)
-				{
-					if(board[x-1][y+1] == type)
-					{
-						for(int k = 1;;++k)
-						{
-							if(x-k < 0 || y+k > 9)
-								break;
-							if(board[x-k][y+k] == 0)
-							{
-								board[x-k][y+k] = 4;
-								break;
-							}
-							else if(board[x-k][y+k] == type){
-								continue;
-							}
-							else {
-								break;
-							}
-						}
-					}
-				}
+				findPathUp(x,y,type);
+				findPathDown(x,y,type);
+				findPathLeft(x,y,type);
+				findPathRight(x,y,type);
+				findPathLeftAndUp(x,y,type);
+				findPathLeftAndDown(x,y,type);
+				findPathRightAndUp(x,y,type);
+				findPathRightAndDown(x,y,type);
 			}
 		}
 	}
